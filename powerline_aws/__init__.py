72bebe03951c196c5798fff1c618733b944eb5b1
Piotr <piotr.golab@klarna.com>
2020-08-21 11:34:08 +0200

    Use correct source of the env variable.

--- powerline_aws/__init__.py
+++ powerline_aws/__init__.py
@@ -1,9 +1,9 @@
-import os
-
 from powerline import PowerlineLogger
 from powerline.theme import requires_segment_info
 
 
 @requires_segment_info
 def aws_profile(pl: PowerlineLogger, segment_info: dict) -> str:
-    return str(os.environ.get('AWS_PROFILE', '@'))
+    profile = str(segment_info['environ'].get('AWS_PROFILE', '@'))
+    (role, account) = profile.split('@')
+    return account or '@'
